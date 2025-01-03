aec7cd88130eabab7818f1a06b57bea09020dfee
Aaron Mavrinac <mavrinac@gmail.com>
2025-01-02 21:39:21 -0500

    Convert tabs to spaces for consistency.

--- software/loader.asm
+++ software/loader.asm
@@ -11,276 +11,276 @@
 ; Modified to load ColecoVision games by J.B. Langston
 ; MegaCart support and other enhancements by Aaron Mavrinac
 
-BOOT:		equ	0			; boot location
-BDOS:		equ	5			; BDOS entry point
-FCB:		equ	$5c			; file control block
-FCBCR:		equ	FCB+$20		; FCB current record
-FCBRN:		equ	FCB+$21		; FCB random access record number
-BUFF:		equ	$80			; DMA buffer
-WRITEC:		equ	2			; BDOS print character function
-WRITESTR:	equ	9			; BDOS print string function
-FOPEN:		equ	15			; BDOS open file function
-FCLOSE:		equ	16			; BDOS close file function
-FREAD:		equ	20			; BDOS sequential read function
-FSIZE:		equ	35			; BDOS compute file size function
+BOOT:       equ 0           ; boot location
+BDOS:       equ 5           ; BDOS entry point
+FCB:        equ $5c         ; file control block
+FCBCR:      equ FCB+$20     ; FCB current record
+FCBRN:      equ FCB+$21     ; FCB random access record number
+BUFF:       equ $80         ; DMA buffer
+WRITEC:     equ 2           ; BDOS print character function
+WRITESTR:   equ 9           ; BDOS print string function
+FOPEN:      equ 15          ; BDOS open file function
+FCLOSE:     equ 16          ; BDOS close file function
+FREAD:      equ 20          ; BDOS sequential read function
+FSIZE:      equ 35          ; BDOS compute file size function
 
-BIOSLEN: 	equ	$2000		; length of BIOS
+BIOSLEN:    equ $2000       ; length of BIOS
 
-MCRPORT:	equ	$0f			; megacart RAM I/O port
-SLOTBASEL:	equ	$bfe0		; slot 0 read address (lower)
-SLOTBASEU:	equ	$ffe0		; slot 0 read address (upper)
+MCRPORT:    equ $0f         ; megacart RAM I/O port
+SLOTBASEL:  equ $bfe0       ; slot 0 read address (lower)
+SLOTBASEU:  equ $ffe0       ; slot 0 read address (upper)
 
-CR:			equ	$0d			; carriage return
-LF:			equ	$0a			; line feed
-EOS:		equ	'$'			; end of string marker
+CR:         equ $0d         ; carriage return
+LF:         equ $0a         ; line feed
+EOS:        equ '$'         ; end of string marker
 
-	org		$0100
+    org     $0100
 
-	ld		(OLDSP),sp		; save old stack pointer
-	ld		sp,STACK		; set new stack pointer
+    ld      (OLDSP),sp      ; save old stack pointer
+    ld      sp,STACK        ; set new stack pointer
 
-	ld		de,FCB			; try to open file
-	ld		c,FOPEN
-	call	BDOS
-	inc		a				; 255 indicates failure
-	jp		z,BADFILE
-	xor		a				; clear current record
-	ld		(FCBCR),a
+    ld      de,FCB          ; try to open file
+    ld      c,FOPEN
+    call    BDOS
+    inc     a               ; 255 indicates failure
+    jp      z,BADFILE
+    xor     a               ; clear current record
+    ld      (FCBCR),a
 
-	ld		de,FCB			; compute file size
-	ld		c,FSIZE
-	call	BDOS
-	ld		hl,(FCBRN)
-	dec		hl
-	ld		a,h
-	cp		1
-	jp		c,REGULAR		; no bank switching necessary for < 32KB
+    ld      de,FCB          ; compute file size
+    ld      c,FSIZE
+    call    BDOS
+    ld      hl,(FCBRN)
+    dec     hl
+    ld      a,h
+    cp      1
+    jp      c,REGULAR       ; no bank switching necessary for < 32KB
 
-	ld		e,28
-	cp		2
-	jr		c,MEGACART		; 64KB megacart
-	ld		e,24
-	cp		4
-	jr		c,MEGACART		; 128KB megacart
-	ld		e,16
-	cp		8
-	jr		c,MEGACART		; 256KB megacart
-	ld		e,0
-	cp		16
-	jr		c,MEGACART		; 512KB megacart
+    ld      e,28
+    cp      2
+    jr      c,MEGACART      ; 64KB megacart
+    ld      e,24
+    cp      4
+    jr      c,MEGACART      ; 128KB megacart
+    ld      e,16
+    cp      8
+    jr      c,MEGACART      ; 256KB megacart
+    ld      e,0
+    cp      16
+    jr      c,MEGACART      ; 512KB megacart
 
-	ld		de,LGFILE		; print error if file is too large
-	ld		c,WRITESTR
-	call	BDOS
-	ld		sp,(OLDSP)		; restore stack pointer
-	ret						; return to CP/M
+    ld      de,LGFILE       ; print error if file is too large
+    ld      c,WRITESTR
+    call    BDOS
+    ld      sp,(OLDSP)      ; restore stack pointer
+    ret                     ; return to CP/M
 
 MEGACART:
-	ld		a,e				; store first slot offset
-	ld		(SLOT0),a
+    ld      a,e             ; store first slot offset
+    ld      (SLOT0),a
 
-	ld		hl,SLOTBASEL	; initialize slot
-	add		hl,de
-	ld		(SLOT),hl
+    ld      hl,SLOTBASEL    ; initialize slot
+    add     hl,de
+    ld      (SLOT),hl
 
-	ld		de,LOADMC		; print loading message
-	ld		c,WRITESTR
-	call	BDOS
+    ld      de,LOADMC       ; print loading message
+    ld      c,WRITESTR
+    call    BDOS
 
-	ld		a,1				; enable lower bank switching
-	out		(MCRPORT),a
+    ld      a,1             ; enable lower bank switching
+    out     (MCRPORT),a
 
-	ld		de,GAMEADDR		; set destination to GAMEADDR for slot 0
-	ld		(DEST),de
+    ld      de,GAMEADDR     ; set destination to GAMEADDR for slot 0
+    ld      (DEST),de
 
 MEGACART_OLOOP:
-	ld		a,128			; set record count to 128 (16KB)
-	ld		(RCOUNT),a
+    ld      a,128           ; set record count to 128 (16KB)
+    ld      (RCOUNT),a
 
-	ld		hl,(SLOT)		; set lower bank slot with dummy read
-	ld		a,(hl)
+    ld      hl,(SLOT)       ; set lower bank slot with dummy read
+    ld      a,(hl)
 
-	ld		a,l				; skip setting destination for slot 0
-	cp		$e0
-	jr		z,MEGACART_ILOOP
+    ld      a,l             ; skip setting destination for slot 0
+    cp      $e0
+    jr      z,MEGACART_ILOOP
 
-	ld		de,$8000		; set destination to lower 16KB bank
-	ld		(DEST),de
+    ld      de,$8000        ; set destination to lower 16KB bank
+    ld      (DEST),de
 
 MEGACART_ILOOP:
-	ld		de,FCB			; read from file
-	ld		c,FREAD
-	call	BDOS
-	or		a
-	jr		nz,MEGACART_EOF
+    ld      de,FCB          ; read from file
+    ld      c,FREAD
+    call    BDOS
+    or      a
+    jr      nz,MEGACART_EOF
 
-	ld		hl,BUFF			; copy from DMA buffer to destination
-	ld		de,(DEST)
-	ld		bc,$80
-	ldir
+    ld      hl,BUFF         ; copy from DMA buffer to destination
+    ld      de,(DEST)
+    ld      bc,$80
+    ldir
 
-	ld		(DEST),de		; increment next destination address
+    ld      (DEST),de       ; increment next destination address
 
-	ld		a,(RCOUNT)		; read up to 16KB
-	dec		a
-	ld		(RCOUNT),a
-	jr		nz,MEGACART_ILOOP
+    ld      a,(RCOUNT)      ; read up to 16KB
+    dec     a
+    ld      (RCOUNT),a
+    jr      nz,MEGACART_ILOOP
 
-	ld		e,'.'			; print slot load character
-	ld		c,WRITEC
-	call	BDOS
+    ld      e,'.'           ; print slot load character
+    ld      c,WRITEC
+    call    BDOS
 
-	ld		de,(SLOT)		; increment slot
-	inc		de
-	ld		a,e				; done if slot LSB rolls over to 0
-	cp		$00
-	jr 		z,MEGACART_EOF
-	ld		(SLOT),de
-	jr		MEGACART_OLOOP
+    ld      de,(SLOT)       ; increment slot
+    inc     de
+    ld      a,e             ; done if slot LSB rolls over to 0
+    cp      $00
+    jr      z,MEGACART_EOF
+    ld      (SLOT),de
+    jr      MEGACART_OLOOP
 
 MEGACART_EOF:
-	ld		e,CR			; print carriage return and linefeed
-	ld		c,WRITEC
-	call	BDOS
-	ld		e,LF
-	ld		c,WRITEC
-	call	BDOS
+    ld      e,CR            ; print carriage return and linefeed
+    ld      c,WRITEC
+    call    BDOS
+    ld      e,LF
+    ld      c,WRITEC
+    call    BDOS
 
-	ld		de,FCB			; close the file
-	ld		c,FCLOSE
-	call	BDOS
+    ld      de,FCB          ; close the file
+    ld      c,FCLOSE
+    call    BDOS
 
-	ld		a,0				; disable lower bank switching
-	out		(MCRPORT),a
+    ld      a,0             ; disable lower bank switching
+    out     (MCRPORT),a
 
-	ld		a,($8000)		; verify magic number
-	cp		$55
-	jr		z,MEGACART_55
-	cp		$aa
-	jr		z,MEGACART_AA
+    ld      a,($8000)       ; verify magic number
+    cp      $55
+    jr      z,MEGACART_55
+    cp      $aa
+    jr      z,MEGACART_AA
 MEGACART_55:
-	ld		a,($8001)
-	cp		$aa
-	jr		z,MEGACART_LOADED
-	jr		MEGACART_FAIL
+    ld      a,($8001)
+    cp      $aa
+    jr      z,MEGACART_LOADED
+    jr      MEGACART_FAIL
 MEGACART_AA:
-	ld		a,($8001)
-	cp		$55
-	jr		z,MEGACART_LOADED
-	jr		MEGACART_FAIL
+    ld      a,($8001)
+    cp      $55
+    jr      z,MEGACART_LOADED
+    jr      MEGACART_FAIL
 
 MEGACART_LOADED:
-	ld		de,SUCCESS		; tell user that game was loaded
-	ld		c,WRITESTR
-	call	BDOS
+    ld      de,SUCCESS      ; tell user that game was loaded
+    ld      c,WRITESTR
+    call    BDOS
 
-	di						; don't need interrupts anymore
+    di                      ; don't need interrupts anymore
 
-	ld		a,(SLOT0)		; enable upper bank switching with mirroring
-	ld		d,0
-	ld		e,a
-	add		a,2
-	out		(MCRPORT),a
+    ld      a,(SLOT0)       ; enable upper bank switching with mirroring
+    ld      d,0
+    ld      e,a
+    add     a,2
+    out     (MCRPORT),a
 
-	ld		hl,SLOTBASEU	; set upper bank to first slot with dummy read
-	add		hl,de
-	ld		a,(hl)
+    ld      hl,SLOTBASEU    ; set upper bank to first slot with dummy read
+    add     hl,de
+    ld      a,(hl)
 
-	ld		a,e				; check if this is a 512KB ROM
-	or		a
-	jr		nz,LB_BOOT		; if not, load ColecoVision BIOS and boot
+    ld      a,e             ; check if this is a 512KB ROM
+    or      a
+    jr      nz,LB_BOOT      ; if not, load ColecoVision BIOS and boot
 
-	ld 		bc,$4000		; move first 16KB of 512KB ROM to slot 0
-	ld 		hl,GAMEADDR+$4000-1
-	ld 		de,$ffff
-	lddr
+    ld      bc,$4000        ; move first 16KB of 512KB ROM to slot 0
+    ld      hl,GAMEADDR+$4000-1
+    ld      de,$ffff
+    lddr
 
-	jr		LB_BOOT			; load ColecoVision BIOS and boot
+    jr      LB_BOOT         ; load ColecoVision BIOS and boot
 
 MEGACART_FAIL:
-	ld		de,MCERR
-	ld		c,WRITESTR
-	call 	BDOS
-	ld		sp,(OLDSP)		; restore stack pointer
-	ret						; return to CP/M
+    ld      de,MCERR
+    ld      c,WRITESTR
+    call    BDOS
+    ld      sp,(OLDSP)      ; restore stack pointer
+    ret                     ; return to CP/M
 
 REGULAR:
-	ld		de,LOADRG		; print loading message
-	ld		c,WRITESTR
-	call	BDOS
+    ld      de,LOADRG       ; print loading message
+    ld      c,WRITESTR
+    call    BDOS
 
-	ld		de,GAMEADDR		; set destination address
-	ld		(DEST),de
+    ld      de,GAMEADDR     ; set destination address
+    ld      (DEST),de
 
 REGULAR_LOOP:
-	ld		de,FCB			; read from file
-	ld		c,FREAD
-	call	BDOS
-	or		a
-	jr		nz,REGULAR_EOF
+    ld      de,FCB          ; read from file
+    ld      c,FREAD
+    call    BDOS
+    or      a
+    jr      nz,REGULAR_EOF
 
-	ld		hl,BUFF			; copy from DMA buffer to destination
-	ld		de,(DEST)
-	ld		bc,$80
-	ldir
-	ld		(DEST),de		; increment next destination address
-	jr		REGULAR_LOOP
+    ld      hl,BUFF         ; copy from DMA buffer to destination
+    ld      de,(DEST)
+    ld      bc,$80
+    ldir
+    ld      (DEST),de       ; increment next destination address
+    jr      REGULAR_LOOP
 
 REGULAR_EOF:
-	ld		de,FCB			; close the file
-	ld		c,FCLOSE
-	call	BDOS
+    ld      de,FCB          ; close the file
+    ld      c,FCLOSE
+    call    BDOS
 
-	ld		de,SUCCESS		; tell user that game was loaded
-	ld		c,WRITESTR
-	call	BDOS
+    ld      de,SUCCESS      ; tell user that game was loaded
+    ld      c,WRITESTR
+    call    BDOS
 
-	di						; don't need interrupts anymore
+    di                      ; don't need interrupts anymore
 
-	ld 		bc,$8000		; copy game to $8000-$FFFF
-	ld 		hl,GAMEADDR+$8000-1
-	ld 		de,$ffff
-	lddr
+    ld      bc,$8000        ; copy game to $8000-$FFFF
+    ld      hl,GAMEADDR+$8000-1
+    ld      de,$ffff
+    lddr
 
 LB_BOOT:
-	ld		bc,LB_END-LB	; move BIOS load code up to GAMEADDR space
-	ld		hl,LB			; (so as not to overwrite it with BIOS)
-	ld		de,GAMEADDR
-	ldir
+    ld      bc,LB_END-LB    ; move BIOS load code up to GAMEADDR space
+    ld      hl,LB           ; (so as not to overwrite it with BIOS)
+    ld      de,GAMEADDR
+    ldir
 
-	jp		GAMEADDR		; run BIOS load and boot
+    jp      GAMEADDR        ; run BIOS load and boot
 
 LB:
-	ld	 	bc,BIOSLEN		; copy ColecoVision BIOS to $0000-$1FFF
-	ld 		hl,BIOS
-	ld 		de,BOOT
-	ldir
-	jp 		BOOT			; jump to BIOS entry point
-LB_END:		equ	$
+    ld      bc,BIOSLEN      ; copy ColecoVision BIOS to $0000-$1FFF
+    ld      hl,BIOS
+    ld      de,BOOT
+    ldir
+    jp      BOOT            ; jump to BIOS entry point
+LB_END:     equ $
 
 BADFILE:
-	ld		de,NOFILE		; print error if file is not found
-	ld		c,WRITESTR
-	call	BDOS
-	ld		sp,(OLDSP)		; restore stack pointer
-	ret						; return to CP/M
+    ld      de,NOFILE       ; print error if file is not found
+    ld      c,WRITESTR
+    call    BDOS
+    ld      sp,(OLDSP)      ; restore stack pointer
+    ret                     ; return to CP/M
 
-NOFILE:		db 	"file not found",CR,LF,EOS
-LGFILE:		db	"file too large",CR,LF,EOS
-LOADRG:		db	"loading regular game",CR,LF,EOS
-LOADMC:		db	"loading MegaCart game",CR,LF,EOS
-MCERR:		db	"error loading MegaCart",CR,LF,EOS
-SUCCESS:	db	"game loaded",CR,LF,EOS
+NOFILE:     db  "file not found",CR,LF,EOS
+LGFILE:     db  "file too large",CR,LF,EOS
+LOADRG:     db  "loading regular game",CR,LF,EOS
+LOADMC:     db  "loading MegaCart game",CR,LF,EOS
+MCERR:      db  "error loading MegaCart",CR,LF,EOS
+SUCCESS:    db  "game loaded",CR,LF,EOS
 
-SLOT0:		db	0			; first slot offset
-SLOT:		dw	0			; slot pointer
-DEST:		dw	GAMEADDR	; destination pointer
-RCOUNT:		db	0			; record counter
-OLDSP:		dw	0			; original stack pointer
-			ds	$40			; space for stack
-STACK:						; top of stack
+SLOT0:      db  0           ; first slot offset
+SLOT:       dw  0           ; slot pointer
+DEST:       dw  GAMEADDR    ; destination pointer
+RCOUNT:     db  0           ; record counter
+OLDSP:      dw  0           ; original stack pointer
+            ds  $40         ; space for stack
+STACK:                      ; top of stack
 
 BIOS:
-	incbin 	"coleco.rom"	; include ColecoVision BIOS in program
+    incbin  "coleco.rom"    ; include ColecoVision BIOS in program
 
-GAMEADDR:					; temporarily load game at end of program
+GAMEADDR:                   ; temporarily load game at end of program
--- software/test.asm
+++ software/test.asm
@@ -1,136 +1,136 @@
-BOOT:		equ	0			; boot location
-BDOS:		equ	5			; BDOS entry point
-WRITEC:		equ	2			; BDOS print character function
-WRITESTR:	equ	9			; BDOS print string function
+BOOT:       equ 0           ; boot location
+BDOS:       equ 5           ; BDOS entry point
+WRITEC:     equ 2           ; BDOS print character function
+WRITESTR:   equ 9           ; BDOS print string function
 
-MCRPORT:	equ	$0f			; megacart RAM I/O port
-SLOTBASEL:	equ	$bfe0		; slot 0 read address (lower)
+MCRPORT:    equ $0f         ; megacart RAM I/O port
+SLOTBASEL:  equ $bfe0       ; slot 0 read address (lower)
 
-CR:			equ	$0d			; carriage return
-LF:			equ	$0a			; line feed
-SPACE:		equ	$20			; space
-EOS:		equ	'$'			; end of string marker
+CR:         equ $0d         ; carriage return
+LF:         equ $0a         ; line feed
+SPACE:      equ $20         ; space
+EOS:        equ '$'         ; end of string marker
 
-	org 	$0100
+    org     $0100
 
-	ld		(OLDSP),sp		; save old stack pointer
-	ld		sp,STACK		; set new stack pointer
+    ld      (OLDSP),sp      ; save old stack pointer
+    ld      sp,STACK        ; set new stack pointer
 
-	ld		de,LOWERMSG
-	ld		c,WRITESTR
-	call	BDOS
+    ld      de,LOWERMSG
+    ld      c,WRITESTR
+    call    BDOS
 
-	ld		a,1				; enable lower bank switching
-	out		(MCRPORT),a
+    ld      a,1             ; enable lower bank switching
+    out     (MCRPORT),a
 
-	ld		de,SLOTBASEL
-	ld		(SLOT),de
+    ld      de,SLOTBASEL
+    ld      (SLOT),de
 
 LOWER_WLOOP:
-	ld	hl,(SLOT)			; set slot with dummy read
-	ld	a,(hl)
+    ld  hl,(SLOT)           ; set slot with dummy read
+    ld  a,(hl)
 
-	ld		a,l				; set a value
-	call	PRINTA
-	ld		($8000),a
-	ld		a,($8000)
-	call	PRINTA
-	ld		e,CR
-	ld		c,WRITEC
-	call	BDOS
-	ld		e,LF
-	ld		c,WRITEC
-	call	BDOS
+    ld      a,l             ; set a value
+    call    PRINTA
+    ld      ($8000),a
+    ld      a,($8000)
+    call    PRINTA
+    ld      e,CR
+    ld      c,WRITEC
+    call    BDOS
+    ld      e,LF
+    ld      c,WRITEC
+    call    BDOS
 
-	ld		de,(SLOT)		; increment the slot
-	inc		de
-	ld		(SLOT),de
-	ld		a,d
-	cp		$c0
-	jr		nz,LOWER_WLOOP
+    ld      de,(SLOT)       ; increment the slot
+    inc     de
+    ld      (SLOT),de
+    ld      a,d
+    cp      $c0
+    jr      nz,LOWER_WLOOP
 
-	ld		de,SLOTBASEL
-	ld		(SLOT),de
+    ld      de,SLOTBASEL
+    ld      (SLOT),de
 
 LOWER_RLOOP:
-	ld		hl,(SLOT)		; set slot with dummy read
-	ld		a,(hl)
+    ld      hl,(SLOT)       ; set slot with dummy read
+    ld      a,(hl)
 
-	ld		a,($8000)		; read and check value
-	call	COMPARE
-	ld		de,(SLOT)		; increment the slot
-	inc		de
-	ld		(SLOT),de
-	ld		a,d
-	cp		$c0
-	jr		nz,LOWER_RLOOP
+    ld      a,($8000)       ; read and check value
+    call    COMPARE
+    ld      de,(SLOT)       ; increment the slot
+    inc     de
+    ld      (SLOT),de
+    ld      a,d
+    cp      $c0
+    jr      nz,LOWER_RLOOP
 
-	ld		a,0				; disable bank switching
-	out		(MCRPORT),a
+    ld      a,0             ; disable bank switching
+    out     (MCRPORT),a
 
-	ei						; re-enable interrupts
+    ei                      ; re-enable interrupts
 
-	ld		sp,(OLDSP)
-	rst		$00
+    ld      sp,(OLDSP)
+    rst     $00
 
 COMPARE:
-	push	hl
-	push	af
-	ld		a,l
-	call	PRINTA
-	pop		af
-	call	PRINTA
-	pop		hl
-	cp		l
-	jr		z,CMP_PASS
-	ld		de,FAIL
-	ld		c,WRITESTR
-	call	BDOS
-	jr		CMP_RET
+    push    hl
+    push    af
+    ld      a,l
+    call    PRINTA
+    pop     af
+    call    PRINTA
+    pop     hl
+    cp      l
+    jr      z,CMP_PASS
+    ld      de,FAIL
+    ld      c,WRITESTR
+    call    BDOS
+    jr      CMP_RET
 CMP_PASS:
-	ld		de,PASS
-	ld		c,WRITESTR
-	call	BDOS
+    ld      de,PASS
+    ld      c,WRITESTR
+    call    BDOS
 CMP_RET:
-	ret
+    ret
 
 PRINTA:
-	push	af
-	push	af
-	rra
-	rra
-	rra
-	rra
-	and		$0f
-	add		a,$30
-	cp		$3a
-	jr		c,PRINTA_M
-	add		a,7
+    push    af
+    push    af
+    rra
+    rra
+    rra
+    rra
+    and     $0f
+    add     a,$30
+    cp      $3a
+    jr      c,PRINTA_M
+    add     a,7
 PRINTA_M:
-	ld		e,a
-	ld		c,WRITEC
-	call	BDOS
-	pop		af
-	and		$0f
-	add		a,$30
-	cp		$3a
-	jr		c,PRINTA_L
-	add		a,7
+    ld      e,a
+    ld      c,WRITEC
+    call    BDOS
+    pop     af
+    and     $0f
+    add     a,$30
+    cp      $3a
+    jr      c,PRINTA_L
+    add     a,7
 PRINTA_L:
-	ld		e,a
-	ld		c,WRITEC
-	call	BDOS
-	ld		e,SPACE
-	ld		c,WRITEC
-	call	BDOS
-	pop		af
-	ret
+    ld      e,a
+    ld      c,WRITEC
+    call    BDOS
+    ld      e,SPACE
+    ld      c,WRITEC
+    call    BDOS
+    pop     af
+    ret
 
-LOWERMSG:	db	"Testing lower banks...",CR,LF,EOS
-PASS:		db	"PASS",CR,LF,EOS
-FAIL:		db	"FAIL",CR,LF,EOS
+LOWERMSG:   db  "Testing lower banks...",CR,LF,EOS
+PASS:       db  "PASS",CR,LF,EOS
+FAIL:       db  "FAIL",CR,LF,EOS
 
-SLOT:		dw	0			; slot pointer
-OLDSP:		dw	0			; original stack pointer
-			ds	$40			; space for stack
-STACK:						; top of stack
+SLOT:       dw  0           ; slot pointer
+OLDSP:      dw  0           ; original stack pointer
+            ds  $40         ; space for stack
+STACK:                      ; top of stack
